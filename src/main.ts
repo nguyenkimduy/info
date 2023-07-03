6a47117a6202f1fcfea73407b677907cdd15776b
nguyenkimduy <46027765+nguyenkimduy@users.noreply.github.com>
2023-07-03 22:39:06 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,51 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, RouterOutlet, Routes } from '@angular/router';
 
-import { AppModule } from './app/app.module';
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  template: '',
+})
+export class App {
+  name = 'Angular';
+}
 
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <h2>home</h2>
+  `,
+})
+export class HomeComponent {}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+@Component({
+  selector: 'not-found',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    not found
+  `,
+})
+export class NotFoundComponent {}
+
+const APP_ROUTES: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+},
+{
+    path: '**',
+    component: NotFoundComponent
+},
+]
+
+bootstrapApplication(App, {
+  providers: [provideRouter(APP_ROUTES)]
+});
