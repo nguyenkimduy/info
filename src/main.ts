ff54c14b136433ac0ebceb9382f4f15caac8d0d2
nguyenkimduy <46027765+nguyenkimduy@users.noreply.github.com>
2023-07-03 23:30:22 +0700

    update code

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,7 @@
-import 'zone.js/dist/zone';
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, RouterOutlet, Routes } from '@angular/router';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-@Component({
-  selector: 'my-app',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: '',
-})
-export class App {
-  name = 'Angular';
-}
+import { AppModule } from './app/app.module';
 
-@Component({
-  selector: 'home',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <h2>home</h2>
-  `,
-})
-export class HomeComponent {}
 
-@Component({
-  selector: 'not-found',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    not found
-  `,
-})
-export class NotFoundComponent {}
-
-const APP_ROUTES: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    component: HomeComponent,
-},
-{
-    path: '**',
-    component: NotFoundComponent
-},
-]
-
-bootstrapApplication(App, {
-  providers: [provideRouter(APP_ROUTES)]
-});
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
